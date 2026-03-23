6ee64ab1ccd56c147ec9f12368923b3f9d884216
Tahssin <143715323+Tahssin-al@users.noreply.github.com>
2026-03-23 10:39:42 +0100

    refactor: remove legacy RBAC test that was flaky and replaced by access-control test

--- lb2-app/todo-list-node/__tests__/rbac.test.js
+++
@@ -1,4 +0,0 @@
-// Legacy test left here intentionally but disabled.
-// It tried to inject sessions into the running app and was flaky.
-// Replaced by `access-control.test.js`.
-test.skip('legacy rbac test (disabled)', () => {});
